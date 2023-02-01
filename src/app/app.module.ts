d575724d6cc78bde5f5247a7f4bb4d8c76af8639
Carol254 <wanguic49@gmail.com>
2023-02-01 22:57:01 +0300

    added few new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { HeaderComponent } from './dashboard/header/header.component';
+import { AboutUsComponent } from './dashboard/about-us/about-us.component';
+import { FeaturesComponent } from './dashboard/features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutUsComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule
